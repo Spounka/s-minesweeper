8969556a015d15b844267685554df12217d5f923
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2021-09-27 12:20:51 +0100

    integrated SFML into project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int
 main(int argc, char **argv) {
-    std::cout << "Hello, World!\n";
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SMinesweeper");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
     return EXIT_SUCCESS;
 }
